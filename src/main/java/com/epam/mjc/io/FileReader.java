2bb76646980b052a79f41fe36d081e43018e6268
djam388 <djam388@hotmail.com>
2022-09-20 15:47:16 +0500

    Attempt #1

--- src/main/java/com/epam/mjc/io/FileReader.java
+++ src/main/java/com/epam/mjc/io/FileReader.java
@@ -1,11 +1,55 @@
 package com.epam.mjc.io;
 
 import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
 
 
 public class FileReader {
 
+
     public Profile getDataFromFile(File file) {
-        return new Profile();
+        String name = "";
+        String email = "";
+        int age = 0;
+        Long phone = 0L;
+        System.out.println(file.toString());
+        try (FileInputStream fileInputStream = new FileInputStream(file)) {
+            String content = "";
+            int ch;
+            while ((ch = fileInputStream.read()) != -1) {
+//                System.out.print((char)ch);
+                content += (char)ch;
+            }
+//            System.out.println(content);
+            String[] contentLines = content.split("\\r?\\n|\\r");
+
+                for (int i = 0; i < contentLines.length; i++) {
+                String[] lineValues = contentLines[i].split(": ");
+                if (lineValues.length > 1)  {
+                    if (lineValues[0].equals("Name")) {
+                        name = lineValues[1];
+                    }
+                    else if (lineValues[0].equals("Age")) {
+                        age = Integer.valueOf(lineValues[1]);
+                    }
+                    else if (lineValues[0].equals("Email")) {
+                        email = lineValues[1];
+                    }
+                    else if (lineValues[0].equals("Phone")) {
+                        phone = Long.valueOf(lineValues[1]);
+                    }
+                }
+            }
+            return new Profile(name, age, email, phone);
+        }
+        catch (FileNotFoundException e) {
+            System.out.println("File not found!");
+        }
+        catch (IOException e) {
+            System.out.println("Something went wrong!");
+        }
+        return null;
     }
 }
---
+++ src/main/java/com/epam/mjc/io/Main.java
@@ -0,0 +1,14 @@
+package com.epam.mjc.io;
+
+import java.io.File;
+
+
+public class Main {
+    public static void main(String[] args) {
+
+        File fileToRead = new File("./" + "/src/main/resources/Profile.txt");
+
+        FileReader fileReader = new FileReader();
+        System.out.println(fileReader.getDataFromFile(fileToRead));
+    }
+}
